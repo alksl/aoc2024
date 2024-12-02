22cf546326da9ff11f721e76257fed1c714b3e8e
Anton Holmgren <me@antonholmgren.com>
2024-12-02 21:51:49 +0100

    Day2: Part 1
    Calculate number of safe diffs

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,18 +1,41 @@
 use std::{fs, env};
 
-fn parse_input(input: &str) -> Vec<Vec<u32>> {
+fn parse_input(input: &str) -> Vec<Vec<i32>> {
     let mut rows = Vec::new();
     for line in input.lines() {
         let trimmed = line.trim();
         if trimmed.is_empty() {
             continue;
         }
-        let row: Vec<u32> = trimmed.split_whitespace()
+        let row: Vec<i32> = trimmed.split_whitespace()
             .map(|x| x.parse().unwrap())
             .collect();
         rows.push(row);
     }
     rows
+}
+
+fn safe_diffs(diffs: &Vec<i32>) -> bool {
+    diffs.iter().all(|&x| x.abs() <= 3)
+}
+
+fn safe_row(row: &Vec<i32>) -> bool {
+    let diffs: Vec<i32> = row.
+        windows(2).
+        map(|window| window[0] - window[1]).
+        collect();
+
+    let all_inreasing = diffs.iter().all(|&x| x > 0);
+    let all_decresing = diffs.iter().all(|&x| x < 0);
+    if all_inreasing || all_decresing {
+        safe_diffs(&diffs)
+    } else {
+        false
+    }
+}
+
+fn num_safe_rows(rows: &Vec<Vec<i32>>) -> i32 {
+    rows.iter().filter(|&row| safe_row(row)).count() as i32
 }
 
 fn main() {
@@ -25,9 +48,8 @@ fn main() {
     match fs::read_to_string(file_path) {
         Ok(content) => {
             let rows = parse_input(&content);
-            for row in &rows {
-                println!("{:?}", row);
-            }
+            let num_safe = num_safe_rows(&rows);
+            println!("{}", num_safe);
         },
         Err(e) => {
             eprintln!("Error reading file: {}", e);
@@ -59,5 +81,28 @@ mod test {
             vec![8, 6, 4, 4, 1],
             vec![1, 3, 6, 7, 9],
         ]);
+    }
+
+    #[test]
+    fn test_num_safe_rows() {
+        let rows = vec![
+            vec![7, 6, 4, 2, 1],
+            vec![1, 2, 7, 8, 9],
+            vec![9, 7, 6, 2, 1],
+            vec![1, 3, 2, 4, 5],
+            vec![8, 6, 4, 4, 1],
+            vec![1, 3, 6, 7, 9],
+        ];
+        assert_eq!(num_safe_rows(&rows), 2);
+    }
+
+    #[test]
+    fn test_safe_row() {
+        assert_eq!(safe_row(&vec![7, 6, 4, 2, 1]), true);
+        assert_eq!(safe_row(&vec![1, 2, 7, 8, 9]), false);
+        assert_eq!(safe_row(&vec![9, 7, 6, 2, 1]), false);
+        assert_eq!(safe_row(&vec![1, 3, 2, 4, 5]), false);
+        assert_eq!(safe_row(&vec![8, 6, 4, 4, 1]), false);
+        assert_eq!(safe_row(&vec![1, 3, 6, 7, 9]), true);
     }
 }
